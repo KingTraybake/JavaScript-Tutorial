f6b50d43cc6d4c4faea3441b877e699430428dfa
Troy <tsmclaughlin09@gmail.com>
2026-03-23 16:12:22 +0000

    Day 01

---
+++ Day01.js
@@ -0,0 +1,35 @@
+// You can use console.log() to print to the console
+console.log("Hello, World!");
+
+// You can use multiple parameters seperated by commas
+console.log("Hello", "World", "!");
+
+// This is a single line comment
+/* This is a
+multiline coment */
+
+// You can do arithmetic operations inside of console.log()
+console.log(2 + 3);
+console.log(3 - 2);
+console.log(3 * 2);
+console.log(3 / 2);
+
+// Their are data types
+console.log(typeof "Hello World!");
+console.log(typeof true);
+console.log(typeof 67);
+console.log(typeof null);
+console.log(typeof undefined);
+
+// You can use variable to store data
+var firstName = "Troy"
+console.log(firstName);
+
+// The let keyword is used to declare variables that can be reassigned
+let num = 5
+num = 3
+console.log(num);
+
+// The const keyword is used to declare variables that cannot be reassigned
+const pi = 3.14
+console.log(pi);
